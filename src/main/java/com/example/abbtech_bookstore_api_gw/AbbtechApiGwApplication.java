4e26ca12a286e423cc78e14fcc8a1791c4b96872
tarxan.piriyev <piriyevtarxan@gmail.com>
2025-03-17 21:08:48 +0400

    copy old version

---
+++ src/main/java/com/example/abbtech_bookstore_api_gw/AbbtechApiGwApplication.java
@@ -0,0 +1,13 @@
+package com.example.abbtech_bookstore_api_gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbtechApiGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbbtechApiGwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/abbtech_bookstore_api_gw/AbbtechBookstoreApiGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abbtech_bookstore_api_gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbtechBookstoreApiGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
